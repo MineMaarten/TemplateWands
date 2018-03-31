985b7af1e0b27f1b5dbdb4dec416c7fa47ec1192
MineMaarten <loopycoaster_8@hotmail.com>
2018-03-31 23:12:32 +0200

    Added inventory items to the required items for copying.

---
+++ src/main/java/com/minemaarten/templatewands/templates/ingredients/providers/ProviderInventoryContents.java
@@ -0,0 +1,39 @@
+package com.minemaarten.templatewands.templates.ingredients.providers;
+
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.fml.common.eventhandler.EventPriority;
+import net.minecraftforge.items.CapabilityItemHandler;
+import net.minecraftforge.items.IItemHandler;
+
+import com.minemaarten.templatewands.api.TemplateWands;
+import com.minemaarten.templatewands.api.ingredients.IIngredientProvider;
+import com.minemaarten.templatewands.api.ingredients.IInputIngredientList;
+import com.minemaarten.templatewands.api.util.BlockContext;
+
+@TemplateWands
+public class ProviderInventoryContents implements IIngredientProvider{
+
+    @Override
+    public EventPriority getPriority(){
+        return EventPriority.NORMAL;
+    }
+
+    @Override
+    public void addIngredients(BlockContext context, IInputIngredientList ingredients){
+        if(context.te != null) {
+            IItemHandler handler = context.te.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, null);
+            if(handler != null) {
+                for(int i = 0; i < handler.getSlots(); i++) {
+                    ItemStack stack = handler.getStackInSlot(i);
+                    if(!stack.isEmpty()) {
+                        ingredients.addItemStackExact(stack);
+                    }
+                }
+
+                //Reset so other ingredient providers can append the inventory block itself
+                ((IngredientList)ingredients).resetChanged(); //TODO cleaner way to allow multiple IIngredientProviders
+            }
+        }
+    }
+
+}
