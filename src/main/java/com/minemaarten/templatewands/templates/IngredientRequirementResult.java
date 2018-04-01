94b400bace12736c2c4a83a45d3783c26407d461
MineMaarten <loopycoaster_8@hotmail.com>
2018-04-01 22:28:25 +0200

    Buckets/tanks are now drained from the inventory to provide fluid requirements.

--- src/main/java/com/minemaarten/templatewands/templates/IngredientRequirementResult.java
+++ src/main/java/com/minemaarten/templatewands/templates/IngredientRequirementResult.java
@@ -3,10 +3,16 @@ package com.minemaarten.templatewands.templates;
 import java.util.function.Predicate;
 
 import net.minecraft.item.ItemStack;
+import net.minecraftforge.fluids.Fluid;
+import net.minecraftforge.fluids.FluidStack;
+import net.minecraftforge.fluids.FluidUtil;
+import net.minecraftforge.fluids.capability.IFluidHandlerItem;
 import net.minecraftforge.items.IItemHandler;
+import net.minecraftforge.items.ItemHandlerHelper;
 import net.minecraftforge.items.ItemStackHandler;
 
 import com.minemaarten.templatewands.templates.ingredients.TemplateIngredient;
+import com.minemaarten.templatewands.templates.ingredients.TemplateIngredientFluidStack;
 import com.minemaarten.templatewands.templates.ingredients.TemplateIngredientItemStack;
 import com.minemaarten.templatewands.templates.ingredients.providers.IngredientList;
 import com.minemaarten.templatewands.util.Log;
@@ -36,7 +42,12 @@ public class IngredientRequirementResult{
         for(int i = 0; i < inventory.getSlots(); i++) {
             ItemStack stack = inventory.getStackInSlot(i);
             if(!stack.isEmpty()) {
-                ingredients.addItemStack(stack);
+                FluidStack fluid = FluidUtil.getFluidContained(stack);
+                if(fluid != null) { //When the item has fluid (like buckets, tanks), provide it as fluid
+                    ingredients.addFluidStack(fluid);
+                } else { //Else as an item
+                    ingredients.addItemStack(stack);
+                }
             }
         }
         return ingredients;
@@ -53,8 +64,14 @@ public class IngredientRequirementResult{
     public void takeItems(){
         for(TemplateIngredient<?> ingredient : ingredients) {
             if(ingredient instanceof TemplateIngredientItemStack) {
-                TemplateIngredientItemStack itemIngredient = ((TemplateIngredientItemStack)ingredient);
+                TemplateIngredientItemStack itemIngredient = (TemplateIngredientItemStack)ingredient;
                 if(!extractItems(inventory, itemIngredient::appliesTo, itemIngredient.getAmount())) {
+                    Log.warning("Could not extract the ingredient " + ingredient + " (dupe bug!)");
+                }
+            }
+            if(ingredient instanceof TemplateIngredientFluidStack) {
+                TemplateIngredientFluidStack fluidIngredient = (TemplateIngredientFluidStack)ingredient;
+                if(!extractFluids(inventory, fluidIngredient.ingredient.getFluid(), fluidIngredient.getAmount())) {
                     Log.warning("Could not extract the ingredient " + ingredient + " (dupe bug!)");
                 }
             }
@@ -72,4 +89,35 @@ public class IngredientRequirementResult{
         }
         return false;
     }
+
+    private static boolean extractFluids(IItemHandler handler, Fluid fluid, int amount){
+        for(int slot = 0; slot < handler.getSlots(); slot++) {
+            ItemStack stack = handler.getStackInSlot(slot);
+            if(!stack.isEmpty()) {
+                ItemStack containerCopy = ItemHandlerHelper.copyStackWithSize(stack, 1); // do not modify the input
+                IFluidHandlerItem containerFluidHandler = FluidUtil.getFluidHandler(containerCopy);
+                if(containerFluidHandler != null) {
+                    FluidStack toExtract = new FluidStack(fluid, amount);
+                    FluidStack extracted = containerFluidHandler.drain(toExtract, true);
+                    if(extracted != null) {
+                        if(handler.extractItem(slot, stack.getCount(), false).getCount() != stack.getCount()) {
+                            Log.warning("Could not extract tank item " + stack);
+                        }
+
+                        if(!containerFluidHandler.getContainer().isEmpty()) {
+                            if(!handler.insertItem(slot, containerFluidHandler.getContainer(), false).isEmpty()) {
+                                Log.warning("Could not insert emtpied tank item " + containerFluidHandler.getContainer());
+                            }
+                        }
+
+                        amount -= extracted.amount;
+                        if(amount <= 0) return true;
+                    }
+
+                }
+            }
+        }
+        return false;
+    }
+
 }
